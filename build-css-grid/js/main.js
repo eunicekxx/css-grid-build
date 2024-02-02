b6ba1cd8d6e295969c0665715b278293c2af9733
Eunice <90015739+xxceanne@users.noreply.github.com>
2024-02-02 09:58:51 -0700

    day 2 commit

---
+++ build-css-grid/js/main.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('show');
+
+    let expanded = this.getAttribute('aria-expanded') == 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+});
